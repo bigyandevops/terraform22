a162bfd13e194d98514dc36ce9f61ce06d18b9bc
bigyandevops <96239841+bigyandevops@users.noreply.github.com>
2022-09-16 13:00:05 +0530

    Create cred.tf

---
+++ cred.tf
@@ -0,0 +1,7 @@
+provider "aws" {
+  shared_config_files      = ["/Users/tf_user/.aws/conf"]
+  shared_credentials_files = ["/Users/tf_user/.aws/creds"]
+  profile                  = "customprofile"
+}
+
+#Shared Configuration and Credentials Files
